c5cc398464c6d40002c72e872729e8def2ef4c41
siamtx <siamtx@yahoo.com>
2026-04-22 22:14:39 -0400

    Prep 1 and Prep 2 complete.  Challenges in Prep 2 complete.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+
+        // Get the first name.
+        Console.Write("What is your first name? ");
+        string First_Name = Console.ReadLine();
+
+        // Get the last name.
+        Console.Write("What is your last name? ");
+        string Last_Name = Console.ReadLine();
+
+        // Provide space before James Bond
+        Console.WriteLine();
+
+        // Print the name James Bond style.
+        Console.WriteLine($"Your name is {Last_Name}, {First_Name} {Last_Name}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,105 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        // Get the grade percentage.
+        Console.WriteLine("Enter your grade percentage:  ");
+        string userInput = Console.ReadLine();
+
+        // Convert string to a float.
+        float percent = int.Parse(userInput);
+
+        // Compare for letter grade.
+        string letter;
+
+        if (percent >= 90)
+        {
+            // Requirement 1 Console.WriteLine("Your letter grade: A");
+            // Requirement 3
+            letter = "A";
+        }
+
+        else if (percent >= 80 && percent <= 89)
+        {
+            // Requirement 1 Console.WriteLine("Your letter grade: B");
+            // Requirement 3
+            letter = "B";
+        }
+
+        else if (percent >= 70 && percent <= 79)
+        {
+            // Requirement 1 Console.WriteLine("Your letter grade: C");
+            // Requirement 3
+            letter = "C";
+        }
+
+        else if (percent >= 60 && percent <= 69)
+        {
+            // Requirement 1 Console.WriteLine("Your letter grade: D");
+            // Requirement 3
+            letter = "D";
+        }
+
+        else
+        {
+            // Requirement 1 Console.WriteLine("Your letter grade: F");
+            // Requirement 3
+            letter = "F";
+        }
+
+        // Challenge 1
+        int extra = (int)percent % 10;
+        string plus;
+        if (extra >= 7)
+        {
+            plus = "+";
+        }
+        else if (extra < 3)
+        {
+            plus = "-";
+        }
+        else
+        {
+            plus = "";
+        }
+
+        // Requirement 3
+        // Console.WriteLine($"Your grade is {letter}.");
+        // Challenge 1
+        // Console.WriteLine($"Your grade is {letter}{plus}.");
+        // Challenge 2
+        if (letter == "A" && plus == "+")
+        {
+            Console.WriteLine($"Your grade is {letter}.");
+        }
+        else if (letter == "A" && plus == "-")
+        {
+            Console.WriteLine($"Your grade is {letter}{plus}.");
+        }
+        else if (letter == "B" && plus == "+" || letter == "C" && plus == "+" || letter == "D" && plus == "+")
+        {
+            Console.WriteLine($"Your grade is {letter}{plus}.");
+        }
+        else if (letter == "B" && plus == "-" || letter == "C" && plus == "-" || letter == "D" && plus == "-")
+        {
+            Console.WriteLine($"Your grade is {letter}{plus}.");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}");
+        }
+
+        // Requirement 2
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations you pass the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry!  You did not pass the course.  Better luck next time!");
+        }
+
+
     }
 }
